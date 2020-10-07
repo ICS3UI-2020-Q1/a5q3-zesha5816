02b96345079828dbc989d4987b6832b618041667
zesha5816 <zesha5816@wrdsb.ca>
2020-10-07 13:53:18 +0000

    finished the example

--- Main.java
+++ Main.java
@@ -12,25 +12,19 @@ public class Main {
   public static void main(String[] args) {
     // Create the Scanner
 		Scanner input = new Scanner(System.in);
-		//prompt user for starting balance
-		  System.out.println("Please enter a starting balance");
-			double startingBalance = input.nextDouble();
-			//prompt user for invest rate
-			System.out.println("Please enter your interest rate");
-			double interestRate  = input.nextInt();
-			//prompt user for investment years
-			System.out.println("Please enter the number of years you will invest for");
-			int investInt = input.nextInt();
-			//initialize count 
-			int count = 0;
-			//turn invest rate in 0.01 value
-			interestRate= interestRate * 0.01;
-			//loops the equation the amount of years for investment
-		 for (int i = 0; i < investInt; i++){
-				startingBalance = startingBalance + (interestRate*startingBalance);
-		 }
-		 //outputs the bala
-		System.out.println("Your final balance would be $" + startingBalance);
-			
+		//output the message to the user
+		System.out.println("Please enter an integer to calculate the factorial of");
+		int factorialValue = input.nextInt();
+		//store intial value as the input number
+		int initialValue = factorialValue;
+		//intialize the final answer as 1
+		int finalAnswer=1;
+		//decrases the value of count by 1 from the inserted value
+		for (int count = factorialValue; count > 0; count--){
+			//multiplys count to final answer 
+			finalAnswer = count * finalAnswer;
+	 }
+	 //outputs the final message
+	 System.out.println(initialValue + "! = " + finalAnswer);
   }
 }
